e248598d64be4183e6e5f6436118a0aff742e53a
Islam Boulila <islamboulila@gmail.com>
2020-08-18 10:01:31 +0100

    Updates

---
+++ precache-manifest.97313bacff09cfcbed9958ea6fdd162f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe5d3a22aee4e01145f1b05ac23bfa7",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "583b8acabbe2eac4eecf",
+    "url": "/my-portfolio/static/css/main.b2b365e4.chunk.css"
+  },
+  {
+    "revision": "d77a5915b721534f5001",
+    "url": "/my-portfolio/static/js/2.1d2ca499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.1d2ca499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583b8acabbe2eac4eecf",
+    "url": "/my-portfolio/static/js/main.881c53be.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "06c8e05aeb458266ade1085896c0332f",
+    "url": "/my-portfolio/static/media/icomoon.06c8e05a.svg"
+  },
+  {
+    "revision": "8275504f5b9e186b54b8d84ebdedc2b4",
+    "url": "/my-portfolio/static/media/icomoon.8275504f.eot"
+  },
+  {
+    "revision": "dd6d51dddf740923fe9c94859aeee2bf",
+    "url": "/my-portfolio/static/media/icomoon.dd6d51dd.woff"
+  },
+  {
+    "revision": "ee8d016f1bfe9220bee450ff02b59624",
+    "url": "/my-portfolio/static/media/icomoon.ee8d016f.ttf"
+  }
+]);
